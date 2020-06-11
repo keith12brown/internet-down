33145251df51fb5443fff76d8cb05eb0470006af
Keith12Brown <keith12brown@msn.com>
2020-06-11 16:01:22 -0600

    Removed all awaits and used callbacks instead.

--- csv.js
+++ csv.js
@@ -11,58 +11,56 @@ if (fs.existsSync(CSV_FILE)) {
     fs.truncateSync(CSV_FILE);
 }
 
+fs.appendFileSync(CSV_FILE, 'id,start,end,attempts,duration\n');
+
 (async () => {
 
-    var db = {};
-    var client = {}
-    try {
-        client = await MongoClient.connect(process.env.MONGO_DB_URL,
-            {
-                useUnifiedTopology: true
-            });
-
-        console.log(`connected to ${client.s.url}`);
-
-        db = client.db();
-    } catch (err) {
-        console.log(`failed connecting to ${process.env.DB_PATH}`);
-        return;
-    }
-
-    const col = await db.collection(process.env.MONGO_COLLECTION);
-
-    fs.appendFileSync(CSV_FILE, 'id,start,end,attempts,duration\n');
-
-    col.aggregate([
-        {
-            $project: {
-                "_id": 1,
-                start: { $dateToString: { format: '%Y-%m-%d %H:%M:%S', timezone: "-06:00", date: "$start" } },
-                end: { $dateToString: { format: '%Y-%m-%d %H:%M:%S', timezone: "-06:00", date: "$end" } },
-                "attempts": 1,
-                duration: { $divide: [{ $subtract: ["$end", "$start"] }, 1000] }
+    MongoClient.connect(process.env.MONGO_DB_URL,
+        { useUnifiedTopology: true },
+        (err, client) => {
+            if (err) {
+                throw err;
             }
-        }
-    ], (err, cursor) => {
-        if (err) {
-            throw err;
-        }
+            console.log(`connected to ${client.s.url}`);
 
-        cursor.on('close', () => {
-            client.close();
-            console.log('done');
-            process.exit(0);
-        })
+            var db = client.db();
 
-        cursor.forEach(data => {
-            if (data) {
-                fs.appendFile(CSV_FILE, `${data._id},${data.start},${data.end},${data.attempts},${data.duration}\n`, (err) => {
-                    if (err) {
-                        console.log(err);
+            db.collection(process.env.MONGO_COLLECTION, (err, col) => {
+                if (err) {
+                    throw err;
+                }
+
+                col.aggregate([
+                    {
+                        $project: {
+                            "_id": 1,
+                            start: { $dateToString: { format: '%Y-%m-%d %H:%M:%S', timezone: "-06:00", date: "$start" } },
+                            end: { $dateToString: { format: '%Y-%m-%d %H:%M:%S', timezone: "-06:00", date: "$end" } },
+                            "attempts": 1,
+                            duration: { $divide: [{ $subtract: ["$end", "$start"] }, 1000] }
+                        }
                     }
-                });
-            }
-        });
-    });
+                ], (err, cursor) => {
+                    if (err) {
+                        throw err;
+                    }
 
+                    cursor.on('close', () => {
+                        client.close();
+                        console.log('done');
+                        process.exit(0);
+                    });
+
+                    cursor.forEach(data => {
+                        if (data) {
+                            fs.appendFile(CSV_FILE, `${data._id},${data.start},${data.end},${data.attempts},${data.duration}\n`, (err) => {
+                                if (err) {
+                                    console.log(err);
+                                }
+                            });
+                        }
+                    });
+                });
+            });
+        });
 })();
